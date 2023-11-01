c1b18f6624664461b1cf2bdb7a13a789a850c248
fepaul <fepaul@students.uni-mainz.de>
2023-11-01 22:24:36 +0100

    multi dimensional works

--- trapezoidal/workflow.jl
+++ trapezoidal/workflow.jl
@@ -11,20 +11,21 @@ function trapezoidal(a, b, f::Function, N=100000)
 end
 
 function multi_dim_trapezoidal(a, b, f, N=100000)
-    res = zeros(f.length)
+    res = zeros(length(f(0)))
     delta_x = (b-a)/N
     x_k_1 = a
     for i in 1:N
-        for j in 1:f.length
-            x_k_1 = a + (i-1)*delta_x 
-            x_k = x_k_1 + delta_x
-            res[j] += (f[j](x_k_1) + f[j](x_k))/2 * delta_x
-        end
+        x_k_1 = a + (i-1)*delta_x 
+        x_k = x_k_1 + delta_x
+        res .+= (f(x_k_1) .+ f(x_k))./2 .* delta_x
     end
     return res
 end
 
 f(x) = cos(200*x)
-#f(x) = (1, x, x^2, x^3)
+function g(x)
+    return (1, x, x^2, x^3)
+end
 
-@show trapezoidal(0,1, f)
+length(g(2))
+@show multi_dim_trapezoidal(0,1, g)
